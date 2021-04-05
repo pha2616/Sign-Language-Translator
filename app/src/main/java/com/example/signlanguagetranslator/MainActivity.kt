9c51abed96e483b3c0799cc1d997cc547df57d21
pha2616 <52243871+pha2616@users.noreply.github.com>
2021-04-05 15:41:04 +0900

    03. main activity

--- app/src/main/java/com/example/signlanguagetranslator/MainActivity.kt
+++ app/src/main/java/com/example/signlanguagetranslator/MainActivity.kt
@@ -2,10 +2,42 @@ package com.example.signlanguagetranslator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
+    var bundle: Bundle = Bundle()
+    override fun onNavigationItemSelected(p0: MenuItem): Boolean {
+        when(p0.itemId){
+            R.id.action_home ->{
+
+                return true
+            }
+            R.id.action_search ->{
+
+            }
+            R.id.action_alarm ->{
+
+            }
+            R.id.action_account ->{
+
+            }
+        }
+        return false
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+        val fragmentTransaction: FragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.main_content, fragment)
+        fragment.arguments = bundle
+        fragmentTransaction.commit()
     }
 }
